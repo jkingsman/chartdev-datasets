88a739986f2924e6243f1a035cf2ac5f7dffd11e
jkingsman <jack.kingsman@gmail.com>
2021-06-08 19:49:15 -0700

    Neater docker push syntax

--- scripts/push_docker.sh
+++ scripts/push_docker.sh
@@ -2,4 +2,4 @@
 set -ex
 
 docker build -t jkingsman/chartdev-datasets:latest -t "jkingsman/chartdev-datasets:$(git rev-parse --short HEAD)" .
-docker push "jkingsman/chartdev-datasets:$(git rev-parse --short HEAD)"
+docker push jkingsman/chartdev-datasets  --all-tags
